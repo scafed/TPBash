75f73c07f772f31cabf8908755aa721ea6f7a07c
scafed <fnscalise@gmail.com>
2026-03-23 18:43:25 -0300

    TP terminado, agrego mejoras, arreglo errores y agrego el txt de alumno de ejemplo

--- menu.sh
+++ menu.sh
@@ -10,15 +10,22 @@ fi
 
 # Caso en que se ingresa el parámetro optativo -d #
 if [ "$1" == "-d" ]; then
-	parametro_optativo=$1
-	
-    # Validamos que se ingrese el nombre del archivo después del parámetro optativo -d #
-    if [ -z "$2" ]; then
-        echo "Debe ingresar el nombre del archivo despues del parámetro -d"
+    echo "Eliminando entorno y cerrando procesos en background..."
+    
+    pkill -f consolidar.sh #Con pkill mata los procesos que coincidan con el nombre, y con -f se busca en el comando completo, no solo en el nombre del proceso.
+    rm -rf ~/EPNro1/ #Con -r (recursivo) se borra el directorio y todo su contenido, y con -f es para que no pida confirmación.
+    
+    echo "Entorno eliminado y procesos en background cerrados con éxito!"
+    
+    # Si ademas del parámetro optativo -d se ingresa el nombre del archivo #
+    if [ -n "$2" ]; then
+        FILENAME=$2
+    
+    # Si solo ingresó el parámetro optativo -d sin el nombre del archivo #
+    else
+        echo "No se ingresó ningún nombre de archivo, cerrando el proceso!"
         exit
     fi
-
-    FILENAME=$2
 
 # Caso en que se ingresa el nombre del archivo sin el parámetro optativo -d #
 else
@@ -26,18 +33,6 @@ else
 fi
 
 export FILENAME
-
-
-### Si se corre el proceso con el parámetro optativo -d, se matará el proceso de consolidar.sh en background y se borrará todo el entorno creado en ~/EPNro1/ ###
-if [ "$parametro_optativo" == "-d" ]; then
-    echo "Eliminando entorno y cerrando procesos en background..."
-    
-    pkill -f consolidar.sh #Con pkill mata los procesos que coincidan con el nombre, y con -f se busca en el comando completo, no solo en el nombre del proceso.
-    rm -rf ~/EPNro1/ #Con -r (recursivo) se borra el directorio y todo su contenido, y con -f es para que no pida confirmación.
-    
-    echo -e "Entorno eliminado y procesos en background cerrados con éxito!\n"
-    exit
-fi
 
 
 ### Menú ###
@@ -55,6 +50,7 @@ do
 	echo "(5) Mostrar datos del alumno por numero de padron"
 	echo "(6) Salir"
 
+    echo -e -n "\tOpción: " #Con -n (nonewline) lo que hace es evitar que agregue una nueva línea después del string.
     read -r opcion #Con -r se evita que se interpreten caracteres de escape como parte del string ingresado por el usuario.
     
     case $opcion in
@@ -64,10 +60,10 @@ do
         echo "Creando entorno..."
         mkdir -p ~/EPNro1/entrada ~/EPNro1/salida ~/EPNro1/procesado #Con -p se crean los directorios si no existen, y si existen no da error.
 
-        if [ [ -d ~/EPNro1/entrada ] && [ -d ~/EPNro1/salida ] && [ -d ~/EPNro1/procesado ] ]; then
-            echo "Entorno creado con éxito!"
+        if [ -d ~/EPNro1/entrada ] && [ -d ~/EPNro1/salida ] && [ -d ~/EPNro1/procesado ]; then
+            echo -e "Entorno creado con éxito!\n"
         else
-            echo "Error al crear el entorno"
+            echo -e "Error al crear el entorno\n"
         fi
     ;;
     
@@ -76,7 +72,7 @@ do
         # Si existe el archivo consolidar.sh se le da permisos y se ejecuta en background.
         if [ -f ~/EPNro1/consolidar.sh ]; then
             chmod +x ~/EPNro1/consolidar.sh #Con chmod +x se le da permisos de ejecución al archivo consolidar.sh
-            echo "Ejecutando el proceso..."
+            echo -e "Ejecutando el proceso...\n"
             ~/EPNro1/consolidar.sh & #Con & se ejecuta el script en segundo plano (background).
         
         else
@@ -92,9 +88,11 @@ do
     for archivo_en_entrada in ~/EPNro1/entrada/*.txt;
     do
         if [ -f "$archivo_en_entrada" ]; then
+            echo -e "\nSe agregó un archivo a la carpeta de entrada!"
             echo "Procesando el archivo $archivo_en_entrada"
-            cat "$archivo_en_entrada" >> ~/EPNro1/salida/"$FILENAME.txt"
+            cat "$archivo_en_entrada" >> ~/EPNro1/salida/"${FILENAME}.txt"
             mv "$archivo_en_entrada" ~/EPNro1/procesado/
+            echo -e "Procesado con éxito!\n"
         fi
     done
 
@@ -103,48 +101,50 @@ done
 fin_del_script_consolidar
         
             chmod +x ~/EPNro1/consolidar.sh
-            echo "Ejecutando el proceso..."
+            echo -e "Ejecutando el proceso...\n"
             ~/EPNro1/consolidar.sh &
         fi
     ;;
 
     ### Opción 3 ###  
     3)
-        if [ -f ~/EPNro1/salida/"$FILENAME.txt" ]; then
-            echo -e "Listado de alumnos ordenados por número de padrón:\n"
-            sort -n -k1 ~/EPNro1/salida/"$FILENAME.txt" #Con -n se ordena numéricamente de menor a mayor, y con -k1 para que arranque desde la primera columna.
+        if [ -f ~/EPNro1/salida/"${FILENAME}.txt" ]; then
+            echo "Listado de alumnos ordenados por número de padrón:"
+            sort -n -k1 ~/EPNro1/salida/"${FILENAME}.txt" #Con -n se ordena numéricamente de menor a mayor, y con -k1 para que arranque desde la primera columna.
+            echo -e -n "\n"
         else
-            echo -e "El archivo no existe en la carpeta de salida\n"
+            echo -e "El archivo ${FILENAME} no existe en la carpeta de salida\n"
         fi
     ;;
 
     ### Opción 4 ###
     4)
-        if [ -f ~/EPNro1/salida/"$FILENAME.txt" ]; then
-            echo -e "Las 10 notas más altas del listado de alumnos:\n"
-            sort -nr -k5 ~/EPNro1/salida/"$FILENAME.txt" | head -n 10 #Con -nr se ordena numéricamente de mayor a menor (r de reverse), y con -k5 para que arranque desde la quinta columna. Con head -n 10 muestro solo las primeras 10 líneas, el -n es para especificar la cantidad de líneas a mostrar.
+        if [ -f ~/EPNro1/salida/"${FILENAME}.txt" ]; then
+            echo "Las 10 notas más altas del listado de alumnos:"
+            sort -nr -k5 ~/EPNro1/salida/"${FILENAME}.txt" | head -n 10 #Con -nr se ordena numéricamente de mayor a menor (r de reverse), y con -k5 para que arranque desde la quinta columna. Con head -n 10 muestro solo las primeras 10 líneas, el -n es para especificar la cantidad de líneas a mostrar.
+            echo -e -n "\n"
         else
-            echo -e "El archivo no existe en la carpeta de salida\n"
+            echo -e "El archivo ${FILENAME} no existe en la carpeta de salida\n"
         fi
     ;;
 
     ### Opción 5 ###
     5)
-        if [ -f ~/EPNro1/salida/"$FILENAME.txt" ]; then
-            echo "Ingrese el número de padrón del alumno: "
+        if [ -f ~/EPNro1/salida/"${FILENAME}.txt" ]; then
+            echo -n "Ingrese el número de padrón del alumno: "
             read -r padron
 
-            busqueda=$(grep "$padron" ~/EPNro1/salida/"$FILENAME.txt")
+            busqueda=$(grep "$padron" ~/EPNro1/salida/"${FILENAME}.txt")
 
             if [ -n "$busqueda" ]; then
-                echo -e "Datos del alumno con número de padrón $padron:\n"
-                echo "$busqueda"
+                echo "Datos del alumno con número de padrón $padron:"
+                echo -e "$busqueda\n"
             else
                 echo -e "No se encontró ningún alumno con el número de padrón $padron\n"
             fi
         
         else
-            echo -e "El archivo no existe en la carpeta de salida\n"
+            echo -e "El archivo ${FILENAME} no existe en la carpeta de salida\n"
         fi
     ;;
 
